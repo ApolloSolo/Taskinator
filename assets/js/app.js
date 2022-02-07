9d473ecd6e25941b350a2bd7fbb3a2da06a47398
ApolloSolo <matt1teixeira@gmail.com>
2022-02-06 19:38:34 -0500

    updating tasks array and saving to localStorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,8 @@ const tasksInProgressEl = document.querySelector("#tasks-in-progress");
 const tasksCompletedEl = document.querySelector("#tasks-completed");
 let taskIdCounter = 0;
 
+let tasks = []
+
 const taskFormHandler = function (event) {
   event.preventDefault();
   const taskNameInput = document.querySelector('input[name="task-name"]').value;
@@ -27,7 +29,8 @@ const taskFormHandler = function (event) {
     else{
       const taskDataObj = {
         name: taskNameInput,
-        type: taskTypeInput
+        type: taskTypeInput,
+        status: "to do"
       }
       createTaskEl(taskDataObj);
     }
@@ -61,6 +64,12 @@ const createTaskEl = function (taskDataObj) {
 
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
+
+  taskDataObj.id = taskIdCounter;
+
+  tasks.push(taskDataObj);
+
+  saveTasks();
 
   taskIdCounter++;
 }
@@ -146,18 +155,40 @@ const completeEditTask = function(taskName, taskType, taskId){
   taskSelected.querySelector("h3.task-name").textContent = taskName;
   taskSelected.querySelector("span.task-type").textContent = taskType;
 
+  // Find the obj that was edited and update it with new data
+  for(let i = 0; i < tasks.length; i++){
+    if(tasks[i].id === parseInt(taskId)){
+      tasks[i].name = taskName;
+      tasks[i].type = taskType;
+    }
+  }
+
+  saveTasks();
+
   alert("Task Updated!");
 
   formEl.removeAttribute("data-task-id");
   document.querySelector("#save-task").textContent = "Add Task";
 
-}
+};
 
 //Delete task
 
 const deletTask = function(taskId){
   let taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
   taskSelected.remove();
+
+   const updatedTaskArray = [];
+
+   for(let i = 0; i < tasks.length; i++){
+     if(tasks[i].id !== parseInt(taskId)){
+       updatedTaskArray.push(tasks[i]);
+     }
+   }
+
+   tasks = updatedTaskArray;
+
+   saveTasks();
 }
 
 //Change our task catigories
@@ -179,7 +210,21 @@ const taskStatusChangeHandler = function(event) {
   else if (statusValue === "completed") {
     tasksCompletedEl.appendChild(taskSelected);
   }
+
+  for(let i = 0; i < tasks.length; i++){
+    if(tasks[i].id === parseInt(taskId)){
+      tasks[i].status = statusValue;
+    }
+  }
+
+  saveTasks();
 };
+
+//Local Storage
+
+const saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 //Event listener
 
